2478c279e8d6e4f27eb0159a4745e88946495c08
Prateek <prateeksanodiya937@gmail.com>
2022-06-12 13:51:30 +0530

    fixed the bug

--- create_cluster.py
+++ create_cluster.py
@@ -7,17 +7,20 @@ def get_ips():
     output.pop(0)
     ips = {} #Stores the hash of Names and IPs of all pods
     for pod in output:
-        pod = pod.split("   ")
+        pod = pod.replace(pod.count(" ")*" ", "@").split("@")
+        print(pod)
         ips[pod[0]] = pod[1]
 
     return ips
 
 sb.run("kubectl apply -f .", shell=True)
 print("waiting for 60 seconds...")
-sleep(60) #wait for 60 seconds for all the redis pods to working
+sleep(35) #wait for 60 seconds for all the redis pods to working
 ips = get_ips()
-
+print(ips)
 cmd = "kubectl exec -it redis-cluster-0  -- redis-cli  --cluster-replicas 2 --cluster create "
+print(cmd)
+
 for pod in ips:
     if "redis-cluster" in pod:
         cmd += ips[pod] + ":6379 "
