901f05f677445d00e1637e883276723a54782940
Prateek <prateeksanodiya937@gmail.com>
2022-06-16 21:41:10 +0530

    modified the script.

--- create_cluster.py
+++ create_cluster.py
@@ -1,5 +1,7 @@
+from configparser import InterpolationMissingOptionError
 import subprocess as sb
 from time import sleep
+import os
 
 def get_ips():
     output = sb.getoutput("kubectl get pods -ocustom-columns=NAME:.metadata.name,IP:.status.podIP")
@@ -13,16 +15,40 @@ def get_ips():
 
     return ips
 
+def cluster_info():
+    sb.run("kubectl exec -it redis-cluster-0  -- redis-cli -c cluster info", shell=True)
+
 sb.run("kubectl apply -f .", shell=True)
-print("waiting for 60 seconds...")
-sleep(35) #wait for 60 seconds for all the redis pods to working
+pods = sb.getoutput("kubectl get pods | grep redis-cluster | wc -l")
+while int(pods) < 9:
+    print("\n" + pods, "pods created")
+    print("Waiting for for all the pods to be up...")
+    sleep(5)
+    pods = sb.getoutput("kubectl get pods | grep redis-cluster | wc -l")
+    if int(pods) == 9:
+        sleep(5)
+        print("All pods created successfully...!")
+
 ips = get_ips()
-print(ips)
-cmd = "kubectl exec -it redis-cluster-0  -- redis-cli  --cluster-replicas 2 --cluster create "
-print(cmd)
+state = sb.getoutput("kubectl exec -it redis-cluster-0  -- redis-cli -c cluster info | grep cluster_state")
+print(state)
+if "ok" not in state:
+    cmd = "kubectl exec -it redis-cluster-0  -- redis-cli  --cluster-replicas 2 --cluster create "
+    
+    for pod in ips:
+        if "redis-cluster" in pod:
+            cmd += ips[pod] + ":6379 "
+    print(cmd)
+    sb.run("echo 'yes' | "+ cmd, shell=True)
+    sleep(2)
+    state = sb.getoutput("kubectl exec -it redis-cluster-0  -- redis-cli -c cluster info | grep cluster_state")
+    if "ok" in state:
+        print("\nCluster created successfully..!\n")
+        cluster_info()
+    else:
+        print("\nFailed...!\n")
+        cluster_info()
+else:   
+    print("\nCluster is already active...!\n")
+    cluster_info()
 
-for pod in ips:
-    if "redis-cluster" in pod:
-        cmd += ips[pod] + ":6379 "
-
-sb.run("echo 'yes' | "+ cmd, shell=True)
